c24bc2753d0431cf25d2b11dedb8df092e53b5cd
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2023-01-11 14:54:01 -0300

    first commit, add: create user route

--- src/index.js
+++ src/index.js
@@ -1,40 +1,61 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+function checksExistsUserAccount(req, res, next) {
+  const { username } = req.body;
+
+	const accountAlreadyExist = users.some((user) => user.username === username);
+
+	if(accountAlreadyExist) {
+		return res.status(400).json({
+			error: 'Usuário já existe'
+		})
+	}
+
+	return next();
 }
 
-app.post('/users', (request, response) => {
+app.post('/users', checksExistsUserAccount, (req, res) => {
+  const { name, username } = req.body;
+
+	const newUser = {
+		id: uuidv4,
+		name,
+		username,
+		todos: []
+	};
+
+	users.push(newUser);
+
+	return res.status(201).json(newUser);
+
+});
+
+app.get('/todos', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.post('/todos', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
+app.put('/todos/:id', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete('/todos/:id', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
